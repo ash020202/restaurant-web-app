37849ae9c468a9eceb3ca30ae36c9d3cc2f2b7e8
ash020202 <vimalofficial02@gmail.com>
2024-06-25 18:43:43 +0530

    build done

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,27 @@
 import "./App.css";
-// src/index.js or src/App.js
 import "./index.css";
-
-// import { Header } from "./components/Header";
-// import EmblaCarousel from "./components/EmblaCarousel";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./Layout/Routes/Home/Home";
 import About from "./Layout/Routes/About/About";
 import Contact from "./Layout/Routes/Contact/Contact";
 
 function App() {
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About about={[]} />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        // {
-        //   path: "/menu",
-        //   element: <Menu />,
-        //   children: [{ path: "/:id", element: <MenuItem /> }],
-        // },
-      ],
-    },
-  ]);
-
   return (
-    <>
-      <RouterProvider router={routes}></RouterProvider>
-    </>
+    <BrowserRouter basename="/restaurant-web-app/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About about={[]} />} />
+          <Route path="contact" element={<Contact />} />
+          {/* Uncomment and adjust the following lines as needed */}
+          {/* <Route path="menu" element={<Menu />}>
+            <Route path=":id" element={<MenuItem />} />
+          </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-  // return (
-  //   <>
-  //     <Header />
-  //     <EmblaCarousel slides={imagelist} />
-  //   </>
-  // );
 }
 
 export default App;
